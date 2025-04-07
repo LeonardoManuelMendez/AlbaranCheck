27822d7650a14601e1c04a805bad66758921142d
Leonardo Méndez <leonardommendez@gmail.com>
2025-04-07 02:19:57 +0200

    saetry

--- V0.1-JAVA-Swing-sin-FrameWorks-sin-BasedeDatos/src/controlador/Controlador.java
+++ V0.1-JAVA-Swing-sin-FrameWorks-sin-BasedeDatos/src/controlador/Controlador.java
@@ -139,7 +139,7 @@ public class Controlador {
 		List<Producto> listaProductos = new ArrayList<>();
 		String codigo_t;
 		String nombre_t;
-		String eanBulto_t;
+		String eanBulto_t; 
 		String eanProducto_t;
 
 		String regex1 = "^(\\d{1,6})\\s{3,}(.{0,32})\\s*(\\d*)\\s*(\\d*)\\s*$"; // Expresión regular
